535d9965565401b07b1dac2549c44eebb6b7741e
astroids <ozgurfall@gmail.com>
2013-10-21 19:40:38 +0300

    coca-cola now its dual core

--- search/mehlib/mehlib.cpp
+++ search/mehlib/mehlib.cpp
@@ -6,6 +6,8 @@
 
 #include "mehlib.h"
 #include <stdexcept>
+#include <future>
+
 /*
 extern "C" int __declspec(dllexport)  addasm(int a, int b);
 
@@ -19,9 +21,25 @@ extern "C" __declspec(dllexport) void adjustASM(unsigned char* norm, unsigned ch
 extern "C"{
 	__declspec(dllexport) void adjustASMadap(unsigned char* _norm, short _bar, INT32 _size, unsigned char * _ori)
 	{
-		adjustASM(_norm, _ori, _bar, _size);
+		unsigned char * n2 = _norm + (_size / 2);
+		unsigned char * o2 = _ori + (_size / 2);
+
+		std::async(std::launch::async, adjustASM,_norm, _ori, _bar, _size/2);
+		std::async(std::launch::async, adjustASM, n2, o2, _bar, _size/2);
+
+		//adjustASM(_norm, _ori, _bar, _size);
+
+
+
 	}
 }
+
+
+
+
+
+
+
 
 extern "C" {void _declspec(dllexport) adjustb(unsigned char*  norm, short bar, INT32 size, unsigned char * ori){
 	for (size_t i = 0; i < size; i++)
